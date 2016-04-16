896a39ed7b307383acd20495eb307eba1a1a49e1
Gal Bracha <galbra@gmail.com>
2016-04-16 16:17:15 +0300

    Removed transition time from settings.

--- bin/data/clean_settings_files.py
+++ bin/data/clean_settings_files.py
@@ -5,8 +5,10 @@ import os
 Clean the settings file from parameters such as
 <fullscreen__F_>1</fullscreen__F_>
 This allows for configuration loaded to not change some of the display modes
+We can use XPath to go to sub parameter such as 
+Transition_time which is a child of settings_transition
 '''
-elementsToRemove =['fullscreen__F_','show_gui__G_']
+elementsToRemove =['fullscreen__F_','show_gui__G_','.//settings_transition/Transition_time']
 def clean():
     currentDir = os.path.dirname(os.path.realpath(__file__))
     print(currentDir)
@@ -20,11 +22,13 @@ def clean():
 def cleanFile(filename):
     tree = ElementTree()
     tree.parse(filename)
+    parent_map = dict((c,p) for p in tree.getiterator() for c in p)
+    
     root = tree.getroot()
     for element in elementsToRemove:
         elementNode = tree.find(element)
         if(elementNode is not None):
-            root.remove(elementNode)
+            parent_map[elementNode].remove(elementNode)
         tree.write(filename)
 
 if __name__ == '__main__':
