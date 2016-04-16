44f3c9cc863c236f3b02885a9f1438e6665d42f8
Gal Bracha <galbra@gmail.com>
2016-04-16 16:04:23 +0300

    Added back the source mode. important apperantly

--- bin/data/clean_settings_files.py
+++ bin/data/clean_settings_files.py
@@ -6,7 +6,7 @@ Clean the settings file from parameters such as
 <fullscreen__F_>1</fullscreen__F_>
 This allows for configuration loaded to not change some of the display modes
 '''
-elementsToRemove =['fullscreen__F_','show_gui__G_','Source_mode__z_']
+elementsToRemove =['fullscreen__F_','show_gui__G_']
 def clean():
     currentDir = os.path.dirname(os.path.realpath(__file__))
     print(currentDir)
@@ -18,14 +18,11 @@ def clean():
         cleanFile(os.path.join(currentDir,filename))
 
 def cleanFile(filename):
-    print (filename)
     tree = ElementTree()
     tree.parse(filename)
     root = tree.getroot()
     for element in elementsToRemove:
-        print(element)
         elementNode = tree.find(element)
-        print(elementNode)
         if(elementNode is not None):
             root.remove(elementNode)
         tree.write(filename)
